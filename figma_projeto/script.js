6699140d96324e0432a6a7fa2cc47a0f7431ee8d
Tobias Fernandes <tobys312@gmail.com>
2023-12-06 11:46:08 -0300

    Init

---
+++ figma_projeto/script.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-login').addEventListener('click', function () {
+    window.location.href = 'login.html';
+});
+
+document.getElementById('btn-cadastro').addEventListener('click', function () {
+    window.location.href = 'cadastro.html';
+});
+
+document.getElementById('logo-site').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('botao-fp').addEventListener('click', function () {
+    window.location.href = 'cadastro.html';
+});
+
+document.getElementById('botao-c4').addEventListener('click', function () {
+    window.location.href = 'cadastro.html';
+});
